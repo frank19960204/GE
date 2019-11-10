e186867023975cb9b6c3fc907f9d494fede642a9
frank19960204 <32299342+frank19960204@users.noreply.github.com>
2019-11-10 17:50:42 +0900

    Initial commit

---
+++ GE_withoutpivoting.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+float linearsystem[5][5] = {{1.0/1.0,1.0/2.0,1.0/3.0,1.0/4.0,1.0/5.0},
+						  	{1.0/2.0,1.0/3.0,1.0/4.0,1.0/5.0,1.0/6.0},
+						  	{1.0/3.0,1.0/4.0,1.0/5.0,1.0/6.0,1.0/7.0},
+						  	{1.0/4.0,1.0/5.0,1.0/6.0,1.0/7.0,1.0/8.0},
+						  	{1.0/5.0,1.0/6.0,1.0/7.0,1.0/8.0,1.0/9.0}};
+float coef[5] = {137.0/60.0,29.0/20.0,153.0/140.0,743.0/840.0,1879.0/2520.0};
+float answer[5] = {0.0,0.0,0.0,0.0,0.0};
+
+void forward(){
+	for(int i = 0;i<4;i++){
+		for(int j = i+1;j<5;j++){
+			//printf("(%d,%d)=%f\n",j,i,);
+			//printf("(%d,%d)=%f\n",i,i,linearsystem[i][i]);
+			float alpha =  linearsystem[j][i]/linearsystem[i][i]; //alpha is first nonzero number divided by pivot
+			//printf("alpha:%f\n",alpha);
+			for(int k = i+1;k<5;k++){
+				linearsystem[j][k] -= linearsystem[i][k] * alpha;	
+			}
+			coef[j] -= coef[i] * alpha;
+			//printf("coef[%d]:%f",j,coef[j]);
+		}
+		for(int x = 0;x<5;x++){
+			for(int y = 0;y<5;y++){
+				printf("%f ",linearsystem[x][y]);
+			}
+			printf("| %f\n",coef[x]);
+		}
+		printf("--------------------------------\n");
+		
+	}
+}
+
+void backward(){
+	for(int i = 4;i>=0;i--){
+		for(int j = 4;j>i;j--){
+			//printf("gp\n");
+			coef[i] -= linearsystem[i][j]*answer[i+1];
+		}
+		//printf("coef[%d]:%f\n",i,coef[4]);
+		answer[i] = coef[i]/linearsystem[i][i];	
+
+	}
+}
+
+void swap(int a,int b){
+	float temp;
+	for(int i = 0;i<5;i++){ 
+ 		temp = linearsystem[a][i];
+ 		linearsystem[a][i] = linearsystem[b][i];
+ 		linearsystem[b][i] = temp;
+ 		}
+}
+
+int main(){
+	forward();
+	backward();
+	for(int i = 0;i<5;i++){
+		printf("x%d:%f\n",i,answer[i]);
+	}
+}
+
+
+
+
---
+++ GEwithpivoting.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+float linearsystem[5][5] = {{1.0/1.0,1.0/2.0,1.0/3.0,1.0/4.0,1.0/5.0},
+						  	{1.0/2.0,1.0/3.0,1.0/4.0,1.0/5.0,1.0/6.0},
+						  	{1.0/3.0,1.0/4.0,1.0/5.0,1.0/6.0,1.0/7.0},
+						  	{1.0/4.0,1.0/5.0,1.0/6.0,1.0/7.0,1.0/8.0},
+						  	{1.0/5.0,1.0/6.0,1.0/7.0,1.0/8.0,1.0/9.0}};
+float coef[5] = {137.0/60.0,29.0/20.0,153.0/140.0,743.0/840.0,1879.0/2520.0};
+float answer[5] = {0.0,0.0,0.0,0.0,0.0};
+
+void swap(int a,int b){
+	float temp;
+	for(int i = 0;i<5;i++){ 
+ 		temp = linearsystem[a][i];
+ 		linearsystem[a][i] = linearsystem[b][i];
+ 		linearsystem[b][i] = temp;
+ 	}
+ 	temp = coef[a];
+ 	coef[a] = coef[b];
+ 	coef[b] = temp;
+}
+
+void pivoting(int a){
+	int biggest = a;
+	for(int i = a;i<5;i++){
+		if(linearsystem[i][a]>=0 || linearsystem[biggest][a]>=0){
+			if(linearsystem[i][a]>linearsystem[biggest][a]){
+				biggest = i;
+			}
+		}
+		else{
+			if(linearsystem[i][a]<linearsystem[biggest][a]){
+				biggest = i;
+			}
+		}
+	}
+	if(biggest != a){
+		swap(biggest,a);
+	}
+}
+
+void forward(){
+	//swap(0,1);
+	
+	for(int i = 0;i<4;i++){
+		pivoting(i);
+		for(int k = 0;k<5;k++){
+			for(int j = 0;j<5;j++){
+				printf("%f ",linearsystem[k][j]);
+			}
+			printf("| %f \n",coef[k]);	
+		}
+		printf("-----------------------------------------------------------------------\n");
+		for(int j = i+1;j<5;j++){
+			float alpha =  linearsystem[j][i]/linearsystem[i][i]; //alpha is first nonzero number divided by pivot
+			for(int k = i+1;k<5;k++){
+				linearsystem[j][k] -= linearsystem[i][k] * alpha;	
+			}
+			coef[j] -= coef[i] * alpha;
+		}
+	}
+}
+
+void backward(){
+	for(int i = 4;i>=0;i--){
+		for(int j = 4;j>i;j--){
+			coef[i] -= linearsystem[i][j]*answer[i+1];
+		}
+		answer[i] = coef[i]/linearsystem[i][i];	
+
+	}
+}
+
+
+
+int main(){
+	forward();
+	backward();
+	for(int i = 0;i<5;i++){
+		printf("x%d:%f\n",i,answer[i]);
+	}
+}
+
+
+
+
